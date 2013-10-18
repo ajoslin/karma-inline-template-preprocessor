5bc6d95641111cc19ceec4779ccce4c3b1a0ee5d
Andy Joslin <andytjoslin@gmail.com>
2013-10-18 13:30:57 -0400

    wip: watch templateUrl files and change js files

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 
 var inlineTemplate = require('inline-template');
+var chokidar = require('chokidar');
+var path = require('path');
+var fs = require('fs');
+
 module.exports = {
   'preprocessor:inlineTemplate': ['factory', inlineTemplatePreprocessor]
 };
 
-function inlineTemplatePreprocessor(options, logger) {
+function inlineTemplatePreprocessor(options, logger, fileList) {
   var log = logger.create('preprocessor.inlineTemplate');
   log.debug('Creating preprocessor with options: base="%s", doubleQuote="%s"', options.base, options.doubleQuote);
   return function(content, file, done) {
     log.debug('Processing file: "%s".', file.originalPath);
-    var compiled = inlineTemplate.process(content, options);
-    done(compiled);
+    var data = inlineTemplate.process(content, options);
+    log.debug('Processed! Templates inlined: ["%s"]', data.templateUrls.join(', '));
+
+    //For each templateUrl included in the js file, when that templateUrl changes update the js file
+    //so it gets re-processed
+    if (data.templateUrls.length) {
+      chokidar.watch(data.templateUrls)
+        .on('change', function(path, stats) {
+          log.debug('inlineTemplate "%s" changed, updating "%s"', path, file.originalPath);
+          fileList.changeFile(file.originalPath);
+        });
+    }
+    
+    done(data.content);
   };
 }
-inlineTemplatePreprocessor.$inject = ['config.inlineTemplatePreprocessor', 'logger'];
+inlineTemplatePreprocessor.$inject = ['config.inlineTemplatePreprocessor', 'logger', 'fileList'];
